6fd1bd487ca1dab67adb651a57066127bfcb8cc9
Oriane <oriane.bacso@cpnv.ch>
2015-05-05 08:51:08 +0200

    test

--- chapitre3/exercice1/formulaire.php
+++ chapitre3/exercice1/formulaire.php
@@ -1,26 +1,27 @@
-
 <!DOCTYPE html>
-<html>
+<html lang="fr">
   <head>
+    <title>Exercice 01</title>
     <meta charset="utf-8">
-    <title>Exercice 1</title>
   </head>
+    <body>
+       <h5>Calcul d'intérêts</h5>
+       <form name="formulaire" method="post" action="traiteformulaire.php">
+        <p>Prix de vente de votre maison :</p>
+        <input name="prixVente" type="number" size="30">
+        <br>
+        <p>Vos fonds propres :</p>
+        <input name="fondPropres" type="number" size="30">
+        <br>
+        <p>Taux 1er rang :</p>
+        <input name="taux1" type="number" size="30" value="3.5">
+        <br>
+        <p>Taux 2ème rand :</p>
+        <input name="taux2" type="number" size="30" value="2.5">
+        <br>
+        <br>
+        <input type="submit" value="Envoyer"/>
+        </form>
 
-  <body>
-  	<form name="formulaire" method="post" action="traitement_formulaire.php">
-      <p>Prix de vente de votre maison :</p>
-      <input name="pv" type="number" size="30">
-      <br>
-      <p>Fonds propres :</p>
-      <input name="fp" type="number">
-      <br>
-      <p>Taux 1er rang :</p>
-      <input name="taux1" type="number" value="3.5">
-      <br>
-      <p>Taux 2eme rand :</p>
-      <input name="taux2" type="number" value="2.5">
-      <br>
-      <input type="submit" value="Envoyer"/>
-    </form>
-  </body>
-</html>
+    </body>
+</html> 
--- chapitre3/exercice1/traiteformulaire.php
+++ chapitre3/exercice1/traiteformulaire.php
@@ -1,25 +1,36 @@
+<?php
+$prixMaison = $_POST['prix'];
+$fondsPropres= $_POST['fonds'];
+?>
+
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Exercie 1 chapitre 3</title>
-  </head>
-  <body>
-    <?php
-	$fondPropre = "";
-	$prixDeVente = "";
-	
-      if ($fondPropre < ($prixDeVente/100*20) {
-        echo "insuffisant";
-      } else {
-        if ($fondPropre >= ($pv/100)*50) {
+    <head>
+        <meta charset="utf-8">
+        <title>Exercice1chapitre3</title>
+    </head>
+    <body>
 
+        <?php
+        if ($fp < ($pv/100)*20) {
+            echo "Attention, vos fonds propres ne sont pas suffisants.";
         } else {
-          $emprunt = ($prixDeVente-$fondPropre);
-
+            if ($fp >= ($pv/100)*50) {
+                $rang1 = 0;
+                $rang2 = ($pv-$fp);
+            } else {
+                $emprunt = ($pv-$fp);
+                $rang1 = (($emprunt/100)*20);
+                $rang2 = ($emprunt-$rang1);
+            }
         }
-
-    ?>
-  </body>
+        if ($fp >= ($pv/100)*20) {
+            echo "Prix de vente : " . $prixVente . " CHF <br>";
+            echo "Fonds propres : " . $fondPropres . " CHF <br>";
+            echo "20% de l'emprunt au taux de " . $taux1 . " : ". $rang1 . "<br>";
+            echo "80% de l'emprunt au taux de " . $taux2 . " : ". $rang2 . "<br>";
+        }
+        ?>
+    </body>
 </html>
