b67004f55a909ff7601b2da31de7201a784371db
Oriane <oriane.bacso@cpnv.ch>
2015-05-07 10:51:02 +0200

    insertion de données

---
+++ insertion_de_donnees/formulaire.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>insertion de données</title>
+    <meta charset="utf-8">
+  </head>
+    <body>
+	   <!--formulaire-->
+       <form name="formulaire" method="post" action="traiteformulaire.php">
+        <p>Entrez votre adresse email: </p>
+        <input name="mail" type="mail" id="mail"> <!--valeur-->
+        <br>
+        <br>
+        <input type="submit" value="Envoyer"/> <!--submit-->
+        </form>
+    </body>
+</html> 
+ 
